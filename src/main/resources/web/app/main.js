68ef9c1ea0bffc80d6b8dcd8b6be0ab9d335f270
jtama <jerome.tama@gmail.com>
2025-05-14 08:31:57 +0200

    How to add custom tag to Roq

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,4 +1,6 @@
 import hljs from 'highlight.js';
 import 'highlight.js/scss/github.scss';
+import mermaid from 'mermaid/dist/mermaid.esm.min.mjs';
 
 hljs.highlightAll();
+mermaid.initialize({ startOnLoad: true });
