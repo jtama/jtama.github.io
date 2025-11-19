d844c852e332c8f33e3daa57a91a54fce362b043
jtama <jerome.tama@gmail.com>
2025-11-19 11:32:09 +0100

    Releasing to maven central using JReleaser

--- src/main/java/Talks.java
+++ src/main/java/Talks.java
@@ -6,12 +6,7 @@ import java.util.List;
 @DataMapping(value = "talks", parentArray = true)
 public record Talks(List<Talk> list) {
 
-    public record Talk(String title, String description, URL slides, URL code, String post,List<Conference> conferences) {
-
-        @Override
-        public String description() {
-            return description;
-        }
+    public record Talk(String title, String description, URL slides, URL code, String post, List<Conference> conferences) {
 
         public record Conference(String name, URL video) {
 
